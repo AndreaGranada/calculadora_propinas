43af3d1cf2089e139bce4a81d5dceb8223b5ebd1
AndreaGranada <andreaalejandre93@gmail.com>
2024-06-01 10:58:55 +0200

    mostrat menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
 import { menuItems } from "./data/db"
+import MenuItem from "./components/MenuItem"
 
 function App() {
 
-console.log(menuItems)
+  console.log(menuItems)
   return (
     <>
       <header className="bg-teal-400 py-5">
         <h1 className="text-center text-4xl font-black">Calculadora de Propinas y Consumo</h1>
       </header>
       <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
-        <div>
-        <h2>Menú</h2>
+        <div className="p-5">
+          <h2 className="font-black text-4xl">Menú</h2>
+          <div className="space-y-3 mt-10">
+            {menuItems.map(item => (
+              <MenuItem key={item.id} item={item} />
+            ))}
+          </div>
         </div>
         <div>
-        <h2>Consumo</h2>
+          <h2>Consumo</h2>
         </div>
-        
+
       </main>
     </>
   )
